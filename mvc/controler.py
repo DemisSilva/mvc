3e103d473dac880c67bd4ade848989692b57eed2
DemisSilva <demisouzasilva@gmail.com>
2022-11-11 10:41:11 -0300

    Incluido conexão com o BD

--- mvc/controler.py
+++ mvc/controler.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, make_response
 from model import Model
-
+from ModelConn import ClienteModel
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
@@ -12,6 +13,14 @@ def consultar():
 @app.route('/v1/aula/consultar/cliente', methods=["GET"])
 def consultarCliente():
     return Model.soma()
+
+@app.route('/v1/aula/cadastrar', methods=["GET"])
+def cadastrarEmail():
+    return ClienteModel.cadastrarEmail()
+
+@app.route('/v1/aula/cadastrar/atualizar', methods=["GET"])
+def atualizar():
+    return ClienteModel.atualizar()
 
 if __name__ == '__main__':
     app.run(debug=True,port= 5000)
